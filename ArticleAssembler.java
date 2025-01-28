46b17706fb735e0338c7b0fbc5ce5e9b9691e158
AlexThuruthel <alexthuruthel04@gmail.com>
2025-01-28 21:51:01 +0530

    Automated code & design smell refactoring

--- ArticleAssembler.java
+++ ArticleAssembler.java
@@ -1,5 +1,5 @@
-package com.sismics.reader.rest.assembler;
-
+```java
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
@@ -8,23 +8,27 @@ import com.sismics.reader.core.dao.jpa.dto.UserArticleDto;
 /**
  * Article DTO / JSON assembler.
  *
- * @author jtremeaux 
+ * @author jtremeaux
  */
 public class ArticleAssembler {
 
     /**
      * Returns a user article as JSON data.
-     * 
+     *
      * @param userArticle User article
      * @return User article as JSON
      */
     public static JSONObject asJson(UserArticleDto userArticle) throws JSONException {
+        JSONObject userArticleJson = createBaseArticleJson(userArticle);
+        addSubscriptionJson(userArticleJson, userArticle);
+        addEnclosureJson(userArticleJson, userArticle);
+        addReadAndStarredStatus(userArticleJson, userArticle);
+        return userArticleJson;
+    }
+
+    private static JSONObject createBaseArticleJson(UserArticleDto userArticle) throws JSONException {
         JSONObject userArticleJson = new JSONObject();
         userArticleJson.put("id", userArticle.getId());
-        JSONObject subscription = new JSONObject();
-        subscription.put("id", userArticle.getFeedSubscriptionId());
-        subscription.put("title", userArticle.getFeedSubscriptionTitle() != null ? userArticle.getFeedSubscriptionTitle() : userArticle.getFeedTitle());
-        userArticleJson.put("subscription", subscription);
         userArticleJson.put("title", userArticle.getArticleTitle());
         userArticleJson.put("url", userArticle.getArticleUrl());
         userArticleJson.put("date", userArticle.getArticlePublicationTimestamp());
@@ -32,6 +36,18 @@ public class ArticleAssembler {
         userArticleJson.put("description", userArticle.getArticleDescription());
         userArticleJson.put("comment_url", userArticle.getArticleCommentUrl());
         userArticleJson.put("comment_count", userArticle.getArticleCommentCount());
+        return userArticleJson;
+    }
+
+    private static void addSubscriptionJson(JSONObject userArticleJson, UserArticleDto userArticle) throws JSONException {
+        JSONObject subscription = new JSONObject();
+        subscription.put("id", userArticle.getFeedSubscriptionId());
+        subscription.put("title", userArticle.getFeedSubscriptionTitle() != null ?
+                userArticle.getFeedSubscriptionTitle() : userArticle.getFeedTitle());
+        userArticleJson.put("subscription", subscription);
+    }
+
+    private static void addEnclosureJson(JSONObject userArticleJson, UserArticleDto userArticle) throws JSONException {
         if (userArticle.getArticleEnclosureUrl() != null) {
             JSONObject enclosure = new JSONObject();
             enclosure.put("url", userArticle.getArticleEnclosureUrl());
@@ -39,8 +55,11 @@ public class ArticleAssembler {
             enclosure.put("type", userArticle.getArticleEnclosureType());
             userArticleJson.put("enclosure", enclosure);
         }
+    }
+
+    private static void addReadAndStarredStatus(JSONObject userArticleJson, UserArticleDto userArticle) throws JSONException {
         userArticleJson.put("is_read", userArticle.getReadTimestamp() != null);
         userArticleJson.put("is_starred", userArticle.getStarTimestamp() != null);
-        return userArticleJson;
     }
 }
+```
