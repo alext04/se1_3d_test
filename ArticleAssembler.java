b447382831cc863ac81e809037abdf053dd6f626
AlexThuruthel <alexthuruthel04@gmail.com>
2025-01-31 21:32:08 +0530

    Automated code & design smell refactoring

--- ArticleAssembler.java
+++ ArticleAssembler.java
@@ -1,65 +1,48 @@
 ```java
+import com.sismics.reader.core.dao.jpa.dto.UserArticleDto;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
-import com.sismics.reader.core.dao.jpa.dto.UserArticleDto;
-
-/**
- * Article DTO / JSON assembler.
- *
- * @author jtremeaux
- */
 public class ArticleAssembler {
 
-    /**
-     * Returns a user article as JSON data.
-     *
-     * @param userArticle User article
-     * @return User article as JSON
-     */
     public static JSONObject asJson(UserArticleDto userArticle) throws JSONException {
-        JSONObject userArticleJson = createBaseArticleJson(userArticle);
-        addSubscriptionJson(userArticleJson, userArticle);
-        addEnclosureJson(userArticleJson, userArticle);
-        addReadAndStarredStatus(userArticleJson, userArticle);
-        return userArticleJson;
+        JSONObject json = new JSONObject();
+        json.put("id", userArticle.getId());
+        json.put("title", userArticle.getArticleTitle());
+        json.put("url", userArticle.getArticleUrl());
+        json.put("date", userArticle.getArticlePublicationTimestamp());
+        json.put("creator", userArticle.getArticleCreator());
+        json.put("description", userArticle.getArticleDescription());
+        json.put("comment_url", userArticle.getArticleCommentUrl());
+        json.put("comment_count", userArticle.getArticleCommentCount());
+        addSubscription(json, userArticle);
+        addEnclosure(json, userArticle);
+        addReadAndStarredStatus(json, userArticle);
+        return json;
     }
 
-    private static JSONObject createBaseArticleJson(UserArticleDto userArticle) throws JSONException {
-        JSONObject userArticleJson = new JSONObject();
-        userArticleJson.put("id", userArticle.getId());
-        userArticleJson.put("title", userArticle.getArticleTitle());
-        userArticleJson.put("url", userArticle.getArticleUrl());
-        userArticleJson.put("date", userArticle.getArticlePublicationTimestamp());
-        userArticleJson.put("creator", userArticle.getArticleCreator());
-        userArticleJson.put("description", userArticle.getArticleDescription());
-        userArticleJson.put("comment_url", userArticle.getArticleCommentUrl());
-        userArticleJson.put("comment_count", userArticle.getArticleCommentCount());
-        return userArticleJson;
-    }
-
-    private static void addSubscriptionJson(JSONObject userArticleJson, UserArticleDto userArticle) throws JSONException {
+    private static void addSubscription(JSONObject json, UserArticleDto userArticle) throws JSONException {
         JSONObject subscription = new JSONObject();
         subscription.put("id", userArticle.getFeedSubscriptionId());
         subscription.put("title", userArticle.getFeedSubscriptionTitle() != null ?
-                userArticle.getFeedSubscriptionTitle() : userArticle.getFeedTitle());
-        userArticleJson.put("subscription", subscription);
+            userArticle.getFeedSubscriptionTitle() : userArticle.getFeedTitle());
+        json.put("subscription", subscription);
     }
 
-    private static void addEnclosureJson(JSONObject userArticleJson, UserArticleDto userArticle) throws JSONException {
+    private static void addEnclosure(JSONObject json, UserArticleDto userArticle) throws JSONException {
         if (userArticle.getArticleEnclosureUrl() != null) {
             JSONObject enclosure = new JSONObject();
             enclosure.put("url", userArticle.getArticleEnclosureUrl());
             enclosure.put("length", userArticle.getArticleEnclosureLength());
             enclosure.put("type", userArticle.getArticleEnclosureType());
-            userArticleJson.put("enclosure", enclosure);
+            json.put("enclosure", enclosure);
         }
     }
 
-    private static void addReadAndStarredStatus(JSONObject userArticleJson, UserArticleDto userArticle) throws JSONException {
-        userArticleJson.put("is_read", userArticle.getReadTimestamp() != null);
-        userArticleJson.put("is_starred", userArticle.getStarTimestamp() != null);
+    private static void addReadAndStarredStatus(JSONObject json, UserArticleDto userArticle) throws JSONException {
+        json.put("is_read", userArticle.getReadTimestamp() != null);
+        json.put("is_starred", userArticle.getStarTimestamp() != null);
     }
 }
 ```
