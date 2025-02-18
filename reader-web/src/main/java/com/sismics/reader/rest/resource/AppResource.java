48539000fb7fc2168491a5b74d0ba2c52e0a9316
AlexThuruthel <alexthuruthel04@gmail.com>
2025-02-18 14:42:44 +0530

    Automated code & design smell refactoring - 2025-02-18 14:42:44

--- reader-web/src/main/java/com/sismics/reader/rest/resource/AppResource.java
+++ reader-web/src/main/java/com/sismics/reader/rest/resource/AppResource.java
@@ -1,3 +1,4 @@
+```java
 package com.sismics.reader.rest.resource;
 
 import com.sismics.reader.core.model.context.AppContext;
@@ -7,7 +8,6 @@ import com.sismics.reader.core.util.jpa.PaginatedLists;
 import com.sismics.reader.rest.constant.BaseFunction;
 import com.sismics.rest.exception.ForbiddenClientException;
 import com.sismics.rest.exception.ServerException;
-import com.sismics.util.NetworkUtil;
 import com.sismics.util.log4j.LogCriteria;
 import com.sismics.util.log4j.LogEntry;
 import com.sismics.util.log4j.MemoryAppender;
@@ -26,14 +26,15 @@ import java.util.ResourceBundle;
 
 /**
  * General app REST resource.
- * 
- * @author jtremeaux
  */
 @Path("/app")
 public class AppResource extends BaseResource {
+
+    private static final String APPENDER_NAME = "MEMORY";
+
     /**
      * Return the information about the application.
-     * 
+     *
      * @return Response
      */
     @GET
@@ -50,10 +51,10 @@ public class AppResource extends BaseResource {
         response.put("free_memory", Runtime.getRuntime().freeMemory());
         return Response.ok().entity(response).build();
     }
-    
+
     /**
      * Retrieve the application logs.
-     * 
+     *
      * @param level Filter on logging level
      * @param tag Filter on logger name / tag
      * @param message Filter on message
@@ -70,29 +71,24 @@ public class AppResource extends BaseResource {
             @QueryParam("message") String message,
             @QueryParam("limit") Integer limit,
             @QueryParam("offset") Integer offset) throws JSONException {
+
         if (!authenticate()) {
             throw new ForbiddenClientException();
         }
+
         checkBaseFunction(BaseFunction.ADMIN);
 
-        // Get the memory appender
-        Logger logger = Logger.getRootLogger();
-        Appender appender = logger.getAppender("MEMORY");
-        if (appender == null || !(appender instanceof MemoryAppender)) {
-            throw new ServerException("ServerError", "MEMORY appender not configured");
-        }
-        MemoryAppender memoryAppender = (MemoryAppender) appender;
-        
-        // Find the logs
-        LogCriteria logCriteria = new LogCriteria()
-                .setLevel(StringUtils.stripToNull(level))
-                .setTag(StringUtils.stripToNull(tag))
-                .setMessage(StringUtils.stripToNull(message));
-        
+        LogCriteria logCriteria = getLogCriteria(level, tag, message);
+
+        MemoryAppender memoryAppender = getAppender(APPENDER_NAME);
+
         PaginatedList<LogEntry> paginatedList = PaginatedLists.create(limit, offset);
+
         memoryAppender.find(logCriteria, paginatedList);
+
         JSONObject response = new JSONObject();
         List<JSONObject> logs = new ArrayList<JSONObject>();
+
         for (LogEntry logEntry : paginatedList.getResultList()) {
             JSONObject log = new JSONObject();
             log.put("date", logEntry.getTimestamp());
@@ -101,56 +97,85 @@ public class AppResource extends BaseResource {
             log.put("message", logEntry.getMessage());
             logs.add(log);
         }
+
         response.put("total", paginatedList.getResultCount());
         response.put("logs", logs);
-        
+
         return Response.ok().entity(response).build();
     }
-    
+
     /**
      * Destroy and rebuild articles index.
-     * 
+     *
      * @return Response
      */
     @POST
     @Path("batch/reindex")
     @Produces(MediaType.APPLICATION_JSON)
     public Response batchReindex() throws JSONException {
+
         if (!authenticate()) {
             throw new ForbiddenClientException();
         }
+
         checkBaseFunction(BaseFunction.ADMIN);
-        
+
         JSONObject response = new JSONObject();
         try {
             AppContext.getInstance().getIndexingService().rebuildIndex();
         } catch (Exception e) {
             throw new ServerException("IndexingError", "Error rebuilding index", e);
         }
+
         response.put("status", "ok");
         return Response.ok().entity(response).build();
     }
-    
+
     /**
-     * Attempt to map a port to the gateway.
-     * 
+     * Map a port to the gateway.
+     *
      * @return Response
      */
     @POST
     @Path("map_port")
     @Produces(MediaType.APPLICATION_JSON)
     public Response mapPort() throws JSONException {
+
         if (!authenticate()) {
             throw new ForbiddenClientException();
         }
+
         checkBaseFunction(BaseFunction.ADMIN);
-        
+
         JSONObject response = new JSONObject();
-        if (!NetworkUtil.mapTcpPort(request.getServerPort())) {
+        if (!mapPort(request.getServerPort())) {
             throw new ServerException("NetworkError", "Error mapping port using UPnP");
         }
-        
+
         response.put("status", "ok");
         return Response.ok().entity(response).build();
     }
+
+    private MemoryAppender getAppender(String appenderName) {
+        Logger logger = Logger.getRootLogger();
+        Appender appender = logger.getAppender(appenderName);
+
+        if (appender == null || !(appender instanceof MemoryAppender)) {
+            throw new ServerException("ServerError", appenderName + " appender not configured");
+        }
+
+        return (MemoryAppender) appender;
+    }
+
+    private boolean mapPort(int port) {
+        return NetworkUtil.mapTcpPort(port);
+    }
+
+    private LogCriteria getLogCriteria(String level, String tag, String message) {
+        return new LogCriteria()
+                .setLevel(StringUtils.stripToNull(level))
+                .setTag(StringUtils.stripToNull(tag))
+                .setMessage(StringUtils.stripToNull(message));
+    }
 }
+```
--- reader-web/src/main/java/com/sismics/reader/rest/resource/LocaleResource.java
+++ reader-web/src/main/java/com/sismics/reader/rest/resource/LocaleResource.java
@@ -1,5 +1,6 @@
-package com.sismics.reader.rest.resource;
+**Refactored Code:**
 
+```java
 import com.sismics.reader.core.dao.jpa.LocaleDao;
 import com.sismics.reader.core.model.jpa.Locale;
 import org.codehaus.jettison.json.JSONException;
@@ -10,19 +11,21 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Locale REST resources.
- * 
+ *
  * @author jtremeaux
  */
 @Path("/locale")
 public class LocaleResource extends BaseResource {
+
+    private static final String LOCALE_ATTR_ID = "id";
+
     /**
      * Returns the list of all locales.
-     * 
+     *
      * @return Response
      */
     @GET
@@ -30,14 +33,36 @@ public class LocaleResource extends BaseResource {
     public Response list() throws JSONException {
         LocaleDao localeDao = new LocaleDao();
         List<Locale> localeList = localeDao.findAll();
+
         JSONObject response = new JSONObject();
-        List<JSONObject> items = new ArrayList<JSONObject>();
-        for (Locale locale : localeList) {
-            JSONObject item = new JSONObject();
-            item.put("id", locale.getId());
-            items.add(item);
-        }
-        response.put("locales", items);
+        response.put("locales", createLocaleItems(localeList));
         return Response.ok().entity(response).build();
     }
+
+    /**
+     * Creates a list of JSON objects representing the locales.
+     *
+     * @param localeList the list of locales
+     * @return the list of JSON objects
+     */
+    private List<JSONObject> createLocaleItems(List<Locale> localeList) throws JSONException {
+        List<JSONObject> items = new ArrayList<>();
+        for (Locale locale : localeList) {
+            items.add(createLocaleItem(locale));
+        }
+        return items;
+    }
+
+    /**
+     * Creates a JSON object representing a locale.
+     *
+     * @param locale the locale
+     * @return the JSON object
+     */
+    private JSONObject createLocaleItem(Locale locale) throws JSONException {
+        JSONObject item = new JSONObject();
+        item.put(LOCALE_ATTR_ID, locale.getId());
+        return item;
+    }
 }
+```
